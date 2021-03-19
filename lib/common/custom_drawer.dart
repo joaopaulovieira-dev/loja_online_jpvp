8d83514ec6400cdab5026bd45303f1383242bee6
joaopaulovieira-dev <joaopaulovieira.dev@gmail.com>
2021-03-19 20:13:09 -0300

    Seção 4 19/03/2021 20:13

---
+++ lib/common/custom_drawer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:loja_online_jpvp/common/drawer_tile.dart';
+
+class CustomDrawer extends StatelessWidget {
+  CustomDrawer(PageController pageController);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerTile(iconData: Icons.home, title: 'Início', page: 0),
+          DrawerTile(iconData: Icons.list, title: 'Produtos', page: 1),
+          DrawerTile(
+              iconData: Icons.playlist_add_check,
+              title: 'Meus Pedidos',
+              page: 2),
+          DrawerTile(iconData: Icons.location_on, title: 'Lojas', page: 3),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:loja_online_jpvp/common/custom_drawer.dart';
+import 'package:loja_online_jpvp/models/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class BaseScreen extends StatelessWidget {
   final PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: pageController,
-      physics: const NeverScrollableScrollPhysics(),
-      children: <Widget>[
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.yellow,
-        ),
-        Container(
-          color: Colors.green,
-        ),
-      ],
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: <Widget>[
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home2'),
+            ),
+          ),
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home3'),
+            ),
+          ),
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home4'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
