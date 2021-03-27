ed6bc452001f2a600e9c902f50c0d794f5884c8b
joaopaulovieira-dev <joaopaulovieira.dev@gmail.com>
2021-03-26 23:56:58 -0300

    S8 39. Selecionando um Tamanho

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_online_jpvp/models/item_size.dart';
 
 class Product {
   Product.fromDocument(DocumentSnapshot document) {
@@ -6,10 +7,16 @@ class Product {
     name = document['name'] as String;
     description = document['description'] as String;
     images = List<String>.from(document.data['images'] as List<dynamic>);
+    sizes = (document.data['sizes'] as List<dynamic> ?? [])
+        .map((s) => ItemSize.fromMap(s as Map<String, dynamic>))
+        .toList();
+
+    print(sizes);
   }
 
   String id;
   String name;
   String description;
   List<String> images;
+  List<ItemSize> sizes;
 }
--- lib/screens/product/product_screen.dart
+++ lib/screens/product/product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_online_jpvp/models/product.dart';
+import 'package:loja_online_jpvp/screens/product/components/size_widget.dart';
 
 class ProductScreen extends StatelessWidget {
   const ProductScreen(this.product);
@@ -71,6 +72,20 @@ class ProductScreen extends StatelessWidget {
                   product.description,
                   style: const TextStyle(fontSize: 16),
                 ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 8),
+                  child: Text(
+                    'Tamanhos',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Wrap(
+                  spacing: 8,
+                  runSpacing: 8,
+                  children: product.sizes.map((s) {
+                    return SizeWidget(size: s);
+                  }).toList(),
+                )
               ],
             ),
           )
