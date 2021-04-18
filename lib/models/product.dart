8ceb174eee679baeac8364c3d53d2aeeb297f04e
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-04-18 02:43:43 -0300

    S15 82. Indicador de Carregamento e Atualizand Produto Local

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:loja_online_jpvp/models/item_size.dart';
+import 'package:uuid/uuid.dart';
 
 class Product extends ChangeNotifier {
   Product({this.id, this.name, this.description, this.images, this.sizes}) {
@@ -18,6 +22,12 @@ class Product extends ChangeNotifier {
         .toList();
   }
 
+  final Firestore firestore = Firestore.instance;
+  final FirebaseStorage storage = FirebaseStorage.instance;
+
+  DocumentReference get firestoreRef => firestore.document('products/$id');
+  StorageReference get storageRef => storage.ref().child('products').child(id);
+
   String id;
   String name;
   String description;
@@ -25,6 +35,13 @@ class Product extends ChangeNotifier {
   List<ItemSize> sizes;
 
   List<dynamic> newImages;
+
+  bool _loading = false;
+  bool get loading => _loading;
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
 
   ItemSize _selectedSize;
   ItemSize get selectedSize => _selectedSize;
@@ -59,6 +76,58 @@ class Product extends ChangeNotifier {
     } catch (e) {
       return null;
     }
+  }
+
+  List<Map<String, dynamic>> exportSizeList() {
+    return sizes.map((size) => size.toMap()).toList();
+  }
+
+  Future<void> save() async {
+    loading = true;
+
+    final Map<String, dynamic> data = {
+      'name': name,
+      'description': description,
+      'sizes': exportSizeList(),
+    };
+
+    if (id == null) {
+      final doc = await firestore.collection('products').add(data);
+      id = doc.documentID;
+    } else {
+      await firestoreRef.updateData(data);
+    }
+
+    final List<String> updateImages = [];
+
+    for (final newImage in newImages) {
+      if (images.contains(newImage)) {
+        updateImages.add(newImage as String);
+      } else {
+        final StorageUploadTask task =
+            storageRef.child(Uuid().v1()).putFile(newImage as File);
+        final StorageTaskSnapshot snapshot = await task.onComplete;
+        final String url = await snapshot.ref.getDownloadURL() as String;
+        updateImages.add(url);
+      }
+    }
+
+    for (final image in images) {
+      if (!newImages.contains(image)) {
+        try {
+          final ref = await storage.getReferenceFromUrl(image);
+          await ref.delete();
+        } catch (e) {
+          debugPrint('Falha ao deletar $image');
+        }
+      }
+    }
+
+    await firestoreRef.updateData({'images': updateImages});
+
+    images = updateImages;
+
+    loading = false;
   }
 
   Product clone() {
