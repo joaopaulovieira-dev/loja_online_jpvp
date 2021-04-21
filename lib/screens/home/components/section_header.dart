b6b9bac673aa581e899464c9be67701c630b11c9
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-04-20 23:41:53 -0300

    S16 85. Adicionando Novas Seções

--- lib/screens/home/components/section_header.dart
+++ lib/screens/home/components/section_header.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:loja_online_jpvp/common/custom_icon_button.dart';
+import 'package:loja_online_jpvp/models/home_manager.dart';
 import 'package:loja_online_jpvp/models/section.dart';
+import 'package:provider/provider.dart';
 
 class SectionHeader extends StatelessWidget {
   const SectionHeader(this.section);
@@ -8,16 +11,45 @@ class SectionHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Text(
-        section.name ?? "Nova Opção",
-        style: const TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.w800,
-          fontSize: 18,
+    final homeManager = context.watch<HomeManager>();
+
+    if (homeManager.editing) {
+      return Row(
+        children: <Widget>[
+          Expanded(
+            child: TextFormField(
+              initialValue: section.name,
+              decoration: const InputDecoration(
+                  hintText: 'Título', isDense: true, border: InputBorder.none),
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+                fontSize: 18,
+              ),
+              onChanged: (text) => section.name = text,
+            ),
+          ),
+          CustomIconButton(
+            iconData: Icons.remove,
+            color: Colors.white,
+            onTap: () {
+              homeManager.removeSection(section);
+            },
+          ),
+        ],
+      );
+    } else {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Text(
+          section.name ?? "Banana",
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w800,
+            fontSize: 18,
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
