b02c5ea97e7d529969b6fa9bd245f65bd7195f7a
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-04-20 23:57:20 -0300

    S16 87. Adicionando Imagens - Parte 1

--- lib/screens/home/components/section_header.dart
+++ lib/screens/home/components/section_header.dart
@@ -5,13 +5,10 @@ import 'package:loja_online_jpvp/models/section.dart';
 import 'package:provider/provider.dart';
 
 class SectionHeader extends StatelessWidget {
-  const SectionHeader(this.section);
-
-  final Section section;
-
   @override
   Widget build(BuildContext context) {
     final homeManager = context.watch<HomeManager>();
+    final section = context.watch<Section>();
 
     if (homeManager.editing) {
       return Row(
