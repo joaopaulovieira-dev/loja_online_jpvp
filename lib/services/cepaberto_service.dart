94e81e7caea739cd5415b3edc8a883251d8d18e2
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-05-13 21:08:58 -0300

    97. Criando um Objeto de Endereço da API

--- lib/services/cepaberto_service.dart
+++ lib/services/cepaberto_service.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:loja_online_jpvp/models/cepaberto_address.dart';
 
 const token = '4545ba98e663b96a4d1ceedb26f16902';
 
 class CepAbertoService {
-  Future<void> getAddressFromCep(String cep) async {
+  Future<CepAbertoAddress> getAddressFromCep(String cep) async {
     final cleanCep = cep.replaceAll('.', '').replaceAll('-', '');
     final endpoint = "https://www.cepaberto.com/api/v3/cep?cep=$cleanCep";
 
@@ -14,14 +15,15 @@ class CepAbertoService {
     dio.options.headers[HttpHeaders.authorizationHeader] = 'Token token=$token';
 
     try {
-      final response = await dio.get<Map>(endpoint);
+      final response = await dio.get<Map<String, dynamic>>(endpoint);
 
       if (response.data.isEmpty) {
         return Future.error('CEP Inválido');
       }
 
-      // ignore: avoid_print
-      print(response.data);
+      final CepAbertoAddress address = CepAbertoAddress.fromMap(response.data);
+
+      return address;
       // ignore: unused_catch_clause
     } on DioError catch (e) {
       return Future.error('Erro ao buscar CEP');
