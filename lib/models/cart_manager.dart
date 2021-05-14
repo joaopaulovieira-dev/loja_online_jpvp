0e1b2df242e2077f0680f7a071d846b7ff02ccae
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-05-14 19:40:08 -0300

    106. Salvando Endereço no Usuário

--- lib/models/cart_manager.dart
+++ lib/models/cart_manager.dart
@@ -30,10 +30,13 @@ class CartManager extends ChangeNotifier {
 
   void updateUser(UserManager userManager) {
     user = userManager.user;
+    productsPrice = 0.0;
     items.clear();
+    removeAddress();
 
     if (user != null) {
       _loadCartItems();
+      _loadUserAddress();
     }
   }
 
@@ -43,6 +46,14 @@ class CartManager extends ChangeNotifier {
     items = cartSnap.documents
         .map((d) => CartProduct.fromDocument(d)..addListener(_onItemUpdated))
         .toList();
+  }
+
+  Future<void> _loadUserAddress() async {
+    if (user.address != null &&
+        await calculateDelivery(user.address.lat, user.address.long)) {
+      address = user.address;
+      notifyListeners();
+    }
   }
 
   void addToCart(Product product) {
@@ -139,6 +150,7 @@ class CartManager extends ChangeNotifier {
     this.address = address;
 
     if (await calculateDelivery(address.lat, address.long)) {
+      user.setAddress(address);
       loading = false;
     } else {
       loading = false;
