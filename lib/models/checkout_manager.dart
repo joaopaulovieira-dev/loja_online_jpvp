4d9c1534e245553ed7d5bbf178ae5024c894adb6
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-05-18 18:56:22 -0300

    113. Enviando o Pedido

--- lib/models/checkout_manager.dart
+++ lib/models/checkout_manager.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:loja_online_jpvp/models/cart_manager.dart';
+import 'package:loja_online_jpvp/models/order.dart';
 import 'package:loja_online_jpvp/models/product.dart';
 
 class CheckoutManager extends ChangeNotifier {
@@ -18,11 +19,17 @@ class CheckoutManager extends ChangeNotifier {
       await _decrementStock();
     } catch (e) {
       onStockFail(e);
-      debugPrint(e.toString());
+      return;
     }
 
-    // ignore: avoid_print
-    _getOrderId().then((value) => print(value));
+    // TODO: PROCESSAR PAGAMENTO
+
+    final orderId = await _getOrderId();
+
+    final order = Order.fromCartManager(cartManager);
+    order.orderId = orderId.toString();
+
+    await order.save();
   }
 
   Future<int> _getOrderId() async {
